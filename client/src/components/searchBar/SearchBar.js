a437c13abe36d7d9115acc99222c6fcaa547a751
KevvKo <kklein915@gmail.com>
2021-06-06 21:48:39 +0200

    add searchbutton

--- client/src/components/searchBar/SearchBar.js
+++ client/src/components/searchBar/SearchBar.js
@@ -1,12 +1,16 @@
 import './SearchBar.css';
+import Button from 'react-bootstrap/Button';
+import {useTranslation} from "react-i18next";
 
 function SearchBar() {
 
-  return (
+    const [t, i18n] = useTranslation('common');
+
+    return (
     <div className="search-bar">
-      
+        <Button variant="secondary" size={'sm'}>{t('searchBar.searchButtonName')}</Button>
     </div>
-  );
+    );
 }
 
 export default SearchBar;
