e5ae61c5d25fcac704b6c5b4888aa8f26da805cf
KevvKo <kklein915@gmail.com>
2021-06-06 21:31:20 +0200

    add searchbar to layout

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -4,6 +4,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 import AppHeader from '../components/header/AppHeader';
+import SearchBar from '../components/searchBar/SearchBar'
 import NavBar from '../components/navbar/NavBar';
 import AppMain from '../components/main/AppMain';
 import FriendsList from '../components/friendsList/FriendsList';
@@ -14,45 +15,51 @@ import ActiveUserList from '../components/activeUserList/ActiveUserList';
 function App() {
 
   return (
-    <div className="App">
-      <Container className="d-flex flex-column h-100" fluid="true">
-        <Row noGutters={true}>
-          <AppHeader />
-        </Row>
-        <Row className="d-flex flex-fill" noGutters={true}>
-          <Col className="d-flex" md={1}>
-            <ChannelList />
-          </Col>
-          <Col className="d-flex">
-            <Row className="flex-fill" noGutters={true}>
-              <Col className="d-flex">
-                <Row className="flex-grow-1" noGutters={true}>
-                  <Col className="d-flex col-auto">
-                    <FriendsList />
-                  </Col>
-                  <Col className="d-flex flex-column">
-                    <Row noGutters={true}>
-                      <Col >
-                        <NavBar />
-                      </Col>
-                    </Row>
-                    <Row className="flex-fill" noGutters= {true}>
-                      <Col className="d-flex">
-                          <AppMain />
-                      </Col>
-                      <Col className="d-flex" md={2}>
-                          <ActiveUserList />
-                      </Col>
-                    </Row>
-                  </Col>
-                </Row>
-              </Col>
-            </Row>
-          </Col>
-        </Row>
-      </Container>
-      
-    </div>
+    <Container className="App d-flex flex-column h-100" fluid="true">
+      <Row noGutters={true}>
+        <AppHeader />
+      </Row>
+      <Row className="d-flex flex-fill" noGutters={true}>
+        <Col className="d-flex" md={1}>
+          <ChannelList />
+        </Col>
+        <Col className="d-flex">
+          <Row className="flex-fill" noGutters={true}>
+            <Col className="d-flex">
+              <Row className="flex-grow-1" noGutters={true}>
+                <Col className="d-flex flex-column col-auto">
+                  <Row noGutters={true}>
+                    <Col>
+                      <SearchBar />
+                    </Col>
+                  </Row>
+                  <Row className="flex-fill" noGutters={true}>
+                    <Col className="d-flex">
+                      <FriendsList />
+                    </Col>
+                  </Row>
+                </Col>
+                <Col className="d-flex flex-column">
+                  <Row noGutters={true}>
+                    <Col >
+                      <NavBar />
+                    </Col>
+                  </Row>
+                  <Row className="flex-fill" noGutters= {true}>
+                    <Col className="d-flex">
+                        <AppMain />
+                    </Col>
+                    <Col className="d-flex" md={2}>
+                        <ActiveUserList />
+                    </Col>
+                  </Row>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
---
+++ client/src/components/searchBar/SearchBar.js
@@ -0,0 +1,12 @@
+import './SearchBar.css';
+
+function SearchBar() {
+
+  return (
+    <div className="search-bar">
+      
+    </div>
+  );
+}
+
+export default SearchBar;
