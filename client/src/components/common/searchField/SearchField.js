0e56682726113683e0ace36c8cc233622ccba5ab
KevvKo <kklein915@gmail.com>
2021-06-07 23:34:00 +0200

    add default property for the searchfield to render an input or button

--- client/src/components/common/searchField/SearchField.js
+++ client/src/components/common/searchField/SearchField.js
@@ -1,15 +1,27 @@
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form'
 import './SearchField.css';
 import {useTranslation} from "react-i18next";
 
 function SearchField(props) {
 
     const [t, i18n] = useTranslation('common');
+    const buttonType = props.type;
+    let searchField;
+
+    if ( buttonType === 'button' ){
+        searchField = <Button className='search-field' variant='flat' size='sm'>{props.placeholder}</Button>
+    } else if( buttonType === 'input' ){
+        searchField = <Form.Control className='search-field' type='text' placeholder={props.placeholder} />
+    }; 
 
     return (
-        
-        <Button className='search-field' variant='flat' size='sm'>{props.placeholder}</Button>
+        searchField
     );
+}
+
+SearchField.defaultProps = {
+    type: 'input'
 }
 
 export default SearchField;
