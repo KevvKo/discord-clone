374140e146b9a17a544fd1902470a372d0c71e34
KevvKo <kklein915@gmail.com>
2021-06-25 23:52:48 +0200

    add first template for the register route

--- client/src/routes/register/Register.js
+++ client/src/routes/register/Register.js
@@ -13,11 +13,33 @@ function Register(props){
 
     return (
         <Row className='register flex-grow-1 justify-content-center align-items-center'>
-            <Col className='p-5' sm ={8}>
-                <h1>{t('login.title')}</h1>
-                <p>{t('login.subGreetings')}</p>
+            <Col className='p-5' sm={8} md={5}>
+                <h1>{t('register.title')}</h1>
                 <Form>
-           
+                    <Form.Group>
+                        <Form.Label>
+                            {t('register.email')}
+                        </Form.Label>
+                        <Form.Control type='email'></Form.Control>
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>
+                            {t('register.username')}
+                        </Form.Label>
+                        <Form.Control type='text'></Form.Control>
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>
+                            {t('register.password')}
+                        </Form.Label>
+                        <Form.Control type="password"></Form.Control>
+                    </Form.Group>
+                    <Button variant='primary' type='button' block>
+                        {t('register.submit')}
+                    </Button> 
+                    <p className='mt-2'>
+                        <Link to='/register'>{t('register.accountAvailable')}</Link>
+                    </p>
                 </Form>
             </Col>
         </Row>
