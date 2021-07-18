26ed7e7cc7709c660574a8d27d68b8a751dbdad0
Kevin Klein <k.klein@intershop.de>
2021-07-18 23:06:36 +0200

    last modificationsfor the authentication process

--- client/src/index.js
+++ client/src/index.js
@@ -12,13 +12,26 @@ import {
   createHttpLink,
   InMemoryCache
 } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
 });
 
+const AUTH_TOKEN = process.env.REACT_APP_AUTH_TOKEN;
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 });
 
