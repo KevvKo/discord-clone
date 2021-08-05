68144749e724c37219b5fa743fcb2d5094e9ae99
KevvKo <kklein915@gmail.com>
2021-08-05 18:06:30 +0200

    rid off unnecessary component

--- client/src/components/forms/structure/formInput/FormInput.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import FormInput from './FormInput';
-
-test('renders learn react link', () => {
-  render(<FormInput />);
-  const linkElement = screen.getByText(/ xyz /i);
-  expect(linkElement).toBeInTheDocument();
-});
