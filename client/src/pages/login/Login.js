c857704d863cafc1c84fb56f58deb258ababbc02
KevvKo <kklein915@gmail.com>
2021-08-18 21:58:23 +0200

    simplify login

--- client/src/pages/login/Login.js
+++ client/src/pages/login/Login.js
@@ -3,82 +3,20 @@ import './Login.css';
 import { 
     Row, 
     Col, 
-    Form, 
-    Button } from 'react-bootstrap';
-import  FormFeedback from '../../components/forms/formFeedback/FormFeedback';
+} from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
-import { 
-    Link, 
-} from 'react-router-dom';
-import useAuth from '../../hooks/useAuthentification';
-import useForm from '../../hooks/useForm';
-import { useHistory } from 'react-router-dom';
-
+import LoginForm from './components/loginForm/LoginForm';
 function Login(){
 
     const [ t ] = useTranslation('common');
-    const history = useHistory();
-    const { 
-        login,
-        handleChange 
-    } = useAuth();
-    const { 
-        errors,  
-        setErrors,
-        validateErrors
-    } = useForm();
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
-        const isValid = validateErrors(e.target);
-
-        if(isValid){
-            login()
-                .then(
-                    ({errors}) => { 
-                        errors ?
-                            setErrors({
-                                email: errors.message,
-                                password: errors.message
-                            }) :
-                            history.replace({
-                                pathname: '/home'
-                            });
-                    });
-        }
-    };
 
     return (
         <Row className='login flex-grow-1 justify-content-center align-items-center'>
             <Col className='p-5' sm={8} md={5}>
                 <h1 className='h1'>{t('login.title')}</h1>
                 <p>{t('login.subGreetings')}</p>
-                <Form noValidate onSubmit={ handleSubmit }>
-                    <FormFeedback error={errors.email}>
-                        <Form.Label>
-                            {t('login.emailLabel')}                       
-                        </Form.Label>
-                        <Form.Control isInvalid={ !!errors.email } onChange={ handleChange } name='email' type='text'></Form.Control>
-                    </FormFeedback>    
-                    <FormFeedback error={errors.password}>
-                        <Form.Label>
-                            {t('login.passwordLabel')}
-                        </Form.Label>
-                        <Form.Control isInvalid={ !!errors.password } onChange={ handleChange } name='password' type='password'></Form.Control>
-                    </FormFeedback>         
-                    <Form.Text>
-                        <Link to='/newPassword'>
-                            {t('login.newPassword')}
-                        </Link>
-                    </Form.Text>
-                    <Button variant='primary' type='submit' block>
-                        {t('login.submit')}
-                    </Button> 
-                    <p className='mt-2'>
-                        {t('login.registrationQuestion')} <Link to='/registration'>{t('login.registration')}</Link>
-                    </p>
-                </Form>
+                <LoginForm />
             </Col>
         </Row>
     );
---
+++ client/src/pages/login/components/loginForm/LoginForm.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { 
+    Form, 
+    Button } from 'react-bootstrap';
+import  FormFeedback from '../../../../components/forms/formFeedback/FormFeedback';
+import { useTranslation } from 'react-i18next';
+import { 
+    Link, 
+} from 'react-router-dom';
+import useAuth from '../../../../hooks/useAuthentification';
+import useForm from '../../../../hooks/useForm';
+import { useHistory } from 'react-router-dom';
+
+function LoginForm(){
+
+    const [ t ] = useTranslation('common');
+    const history = useHistory();
+    const { 
+        login,
+        handleChange 
+    } = useAuth();
+    const { 
+        errors,  
+        setErrors,
+        validateErrors
+    } = useForm();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const isValid = validateErrors(e.target);
+
+        if(isValid){
+            login()
+                .then(
+                    ({errors}) => { 
+                        errors ?
+                            setErrors({
+                                email: errors.message,
+                                password: errors.message
+                            }) :
+                            history.replace({
+                                pathname: '/home'
+                            });
+                    });
+        }
+    };
+
+    return (
+        <Form noValidate onSubmit={ handleSubmit }>
+            <FormFeedback error={errors.email}>
+                <Form.Label>
+                    {t('login.emailLabel')}                       
+                </Form.Label>
+                <Form.Control isInvalid={ !!errors.email } onChange={ handleChange } name='email' type='text'></Form.Control>
+            </FormFeedback>    
+            <FormFeedback error={errors.password}>
+                <Form.Label>
+                    {t('login.passwordLabel')}
+                </Form.Label>
+                <Form.Control isInvalid={ !!errors.password } onChange={ handleChange } name='password' type='password'></Form.Control>
+            </FormFeedback>         
+            <Form.Text>
+                <Link to='/newPassword'>
+                    {t('login.newPassword')}
+                </Link>
+            </Form.Text>
+            <Button variant='primary' type='submit' block>
+                {t('login.submit')}
+            </Button> 
+            <p className='mt-2'>
+                {t('login.registrationQuestion')} <Link to='/registration'>{t('login.registration')}</Link>
+            </p>
+        </Form>
+    );
+}
+
+export default LoginForm;
