fe5204461681250e65672fa813aa79373fd8a14f
KevvKo <kklein915@gmail.com>
2021-08-30 21:02:11 +0200

    extending settings navbar

--- client/src/pages/settings/components/settingsNavbar/SettingsNavbar.js
+++ client/src/pages/settings/components/settingsNavbar/SettingsNavbar.js
@@ -15,24 +15,37 @@ function SettingsNavbar(){
     return(
         <Col className='settings-navbar' md={ 3 }>
             <div className='mt-5'>
-                <h6 className='ml-1'>
-                    {t('settings.navbar.userSettingsTitle')}
-                </h6>
-                <ListTab>
-                    {t('settings.navbar.myAccount')}
-                </ListTab>
-                <ListTab>
-                    {t('settings.navbar.userProfile')}
-                </ListTab>
-                <ListTab>
-                    {t('settings.navbar.privacyAndSecurity')}
-                </ListTab>
-                <ListTab>
-                    {t('settings.navbar.authorizedApps')}
-                </ListTab>
-                <ListTab>
-                    {t('settings.navbar.connections')}
-                </ListTab>
+                <div className='pb-3 settings-navbar-separator'>
+                    <h6 className='ml-1'>
+                        {t('settings.navbar.userSettingsTitle')}
+                    </h6>
+                    <ListTab>
+                        {t('settings.navbar.myAccount')}
+                    </ListTab>
+                    <ListTab>
+                        {t('settings.navbar.userProfile')}
+                    </ListTab>
+                    <ListTab>
+                        {t('settings.navbar.privacyAndSecurity')}
+                    </ListTab>
+                    <ListTab>
+                        {t('settings.navbar.authorizedApps')}
+                    </ListTab>
+                    <ListTab>
+                        {t('settings.navbar.connections')}
+                    </ListTab>
+                </div>
+                <div className="mt-3 pb-3 settings-navbar-separator">
+                    <h6 className='ml-1'>
+                        {t('settings.navbar.activitySettings')}
+                    </h6>
+                    <ListTab>
+                        {t('settings.navbar.activityStatus')}
+                    </ListTab>
+                    <ListTab>
+                        {t('settings.navbar.gameOverlay')}
+                    </ListTab>
+                </div>
             </div>
         </Col>
     );
