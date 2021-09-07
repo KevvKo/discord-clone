32400e7d6e0ceeffd8dd11ef3626a526a52db29b
KevvKo <kklein915@gmail.com>
2021-09-07 21:44:25 +0200

    update content for userdata settings

--- client/src/pages/myAccount/components/userDataSettings/UserDataSettings.js
+++ client/src/pages/myAccount/components/userDataSettings/UserDataSettings.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+// Components
+import { Button } from 'react-bootstrap';
 // Hooks 
 import { useTranslation } from 'react-i18next';
 
@@ -8,7 +10,16 @@ function UserDataSettings(){
     const [ t ] = useTranslation('common');
 
     return(
-        <div className='user-data-settings'>
+        <div className='user-data-settings px-3 pb-3'>
+            <div className='d-flex mb-4'>
+                <Button className='ml-auto' variant='secondary'>{t('settings.main.myAccount.edit')}</Button>
+            </div>
+            <div className='d-flex mb-4'>
+                <Button className='ml-auto' variant='secondary'>{t('settings.main.myAccount.edit')}</Button>
+            </div>
+            <div className='d-flex mb-4'>
+                <Button className='ml-auto' variant='secondary'>{t('settings.main.myAccount.add')}</Button>
+            </div>
         </div>
     );
 }
