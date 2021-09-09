653300091a26688950671c1f5382a9987734400a
KevvKo <kklein915@gmail.com>
2021-09-09 22:53:56 +0200

    update layout for DeleteAccount

--- client/src/pages/myAccount/components/deleteAccount/DeleteAccount.js
+++ client/src/pages/myAccount/components/deleteAccount/DeleteAccount.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './DeleteAccount.css';
 
 // Components
-import { Col } from 'react-bootstrap';
+import { Button, Col } from 'react-bootstrap';
 // Hooks
 import { useTranslation } from 'react-i18next';
 
@@ -11,7 +11,13 @@ function DeleteAccount(){
     const [ t ] = useTranslation('common');
 
     return(
-        <Col className='delete-account flex-column mt-5 px-4'>
+        <Col className='delete-account flex-column mt-4 px-0'>
+            <h6>{t('settings.main.myAccount.deleteAccount')}</h6>
+            <p>{t('settings.main.myAccount.deactivateAccountDescription')}</p>
+            <div>
+                <Button variant='danger' className='mr-3'>{t('settings.main.myAccount.deactivateAccount')}</Button>
+                <Button variant='outline-danger'>{t('settings.main.myAccount.deleteAccount')}</Button>
+            </div>
         </Col>
     );
 }
