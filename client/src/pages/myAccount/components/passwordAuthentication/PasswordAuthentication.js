daa72d01057ecba7b9d962ee0820fd06860d0e5e
KevvKo <kklein915@gmail.com>
2021-09-09 22:38:56 +0200

    update layout for PasswordAuthentication

--- client/src/pages/myAccount/components/passwordAuthentication/PasswordAuthentication.js
+++ client/src/pages/myAccount/components/passwordAuthentication/PasswordAuthentication.js
@@ -2,16 +2,19 @@ import React from 'react';
 import './PasswordAuthentication.css';
 
 // Components
-import { Col } from 'react-bootstrap';
+import { Button, Col } from 'react-bootstrap';
 // Hooks
 import { useTranslation } from 'react-i18next';
 
 function PasswordAuthentication(){
-
     const [ t ] = useTranslation('common');
 
     return(
-        <Col className='password-authentication flex-column mt-5 px-4'>
+        <Col className='password-authentication flex-column mt-5 px-0'>
+            <Button className='mb-4' variant='primary' size={'sm'} >{t('settings.main.myAccount.changePassword')}</Button>
+            <h6>{t('settings.main.myAccount.twoFactorTitle')}</h6>
+            <p className='mb-4' >{t('settings.main.myAccount.twoFactorDescription')}</p>
+            <Button className='mb-4' variant='primary' size={'sm'} >{t('settings.main.myAccount.activateTwoFactor')}</Button>
         </Col>
     );
 }
