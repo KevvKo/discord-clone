00ebcbeb873952015e32c0b5983c501dee87f051
KevvKo <kklein915@gmail.com>
2021-09-15 22:05:24 +0200

    add mutation addPhoneNumber

--- api/src/resolvers/Mutation.js
+++ api/src/resolvers/Mutation.js
@@ -57,6 +57,16 @@ async function signup( parent, args, context, info ) {
     return updatedUser
   }
 
+  async function addPhoneNumber( parent, args, context, info ){
+    const { userId } = context;
+    const updatedUser = await context.prisma.user.update({
+      where: { id: userId },
+      data: { phoneNumber: args.phoneNumber } 
+    })
+
+    return updatedUser;
+  }
+
   async function changePassword( parent, args, context, info ){
 
     const saltRounds =  5;
@@ -135,6 +145,7 @@ async function signup( parent, args, context, info ) {
   }
 
   module.exports = {
+    addPhoneNumber,
     changeActive,
     changePassword,
     changeEmail,
