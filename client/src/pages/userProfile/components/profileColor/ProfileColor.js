46d89641e9147c0031dd8edb2f288b4000679b66
KevvKo <kklein915@gmail.com>
2021-09-20 23:18:34 +0200

    add colorpicker to profileUser

--- client/src/pages/userProfile/components/profileColor/ProfileColor.js
+++ client/src/pages/userProfile/components/profileColor/ProfileColor.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import './ProfileColor.css';
-// Components
-import { Button } from 'react-bootstrap';
 // Hooks 
 import { useTranslation } from 'react-i18next';
+import { useState, useEffect } from 'react';
 
 function ProfileColor(){
 
     const [ t ] = useTranslation('common');
+    const [ customizedColor, setCustomizedColor ] = useState(''); 
 
+    const handleInput = (e) => {
+        console.log('jo');
+        setCustomizedColor(e.target.value);
+    };
+
+    useEffect( () => {
+        document.querySelector('.color-panel-customized').style.background = customizedColor;
+    }, [customizedColor]);
     return(
         <div className='profile-color'>
             <div className='subtitle mb-3'>
@@ -20,7 +28,9 @@ function ProfileColor(){
                     <p className='text-center'>{t('settings.main.userProfile.default')}</p>
                 </div>
                 <div>
-                    <div className='color-panel-customized'></div>
+                    <div className='color-panel-customized'>
+                        <input id='color-picker' type='color' onInput={ handleInput } onChange={ handleInput }></input>
+                    </div>
                     <p className='text-center'>{t('settings.main.userProfile.customized')}</p>
                 </div>
             </div>
