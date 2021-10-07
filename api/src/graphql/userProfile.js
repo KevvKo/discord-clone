372e8b1b15e23837206dd3c794e03a3baadd5659
KevvKo <kklein915@gmail.com>
2021-10-07 22:58:13 +0200

    add mutation to upload images

--- api/src/graphql/userProfile.js
+++ api/src/graphql/userProfile.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
 const { gql } = require('apollo-server-express');
+const { GraphQLUpload } = require('graphql-upload');
+const fs = require('fs');
 
 const UserProfile = gql`
+
+scalar Upload
 
 type UserProfile {
     userId: ID!
@@ -10,15 +14,24 @@ type UserProfile {
     path: String
 }
 
+type Image{
+    filename: String!
+    mimetype: String!
+    encoding: String!
+}
+
 extend type Mutation {
     setColor (color: String): UserProfile!
     setDescription (description: String): UserProfile!
     setPath (string: String): UserProfile!
+    uploadImage (image: Upload! ): Image!
 }`
 
 const userProfileResolvers = {
+    Upload: GraphQLUpload,
+
     Mutation: {
-        setColor: async ( parent, args, context, info ) => {
+        setColor: async ( parent, args, context ) => {
             const { userId } = context;
             const updatedUserProfile = await context.prisma.userprofile.update({
                 where: { userId: userId },
@@ -27,7 +40,7 @@ const userProfileResolvers = {
 
             return updatedUserProfile;
         },
-        setDescription: async ( parent, args, context, info ) => {
+        setDescription: async ( parent, args, context ) => {
             const { userId } = context;
             const updatedUserProfile = await context.prisma.userprofile.update({
                 where: { userId: userId },
@@ -36,7 +49,7 @@ const userProfileResolvers = {
 
             return updatedUserProfile;
         },
-        setPath: async ( parent, args, context, info ) => {
+        setPath: async ( parent, args, context ) => {
             const { userId } = context;
             const updatedUserProfile = await context.prisma.userprofile.update({
                 where: { userId: userId },
@@ -44,6 +57,12 @@ const userProfileResolvers = {
             })
 
             return updatedUserProfile;
+        },
+        uploadImage: async( parent, { file }) => {
+            const { filename, mimetype, createReadStream } = await image;
+            const stream = createReadStream();
+            const writeStream = fs.createWriteStream('./src/assets/img');
+            return true;
         }
     }
 }
