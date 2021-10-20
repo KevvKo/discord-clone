d99e5c1a44ec243c452769eceba25d4b8e4365db
KevvKo <kklein915@gmail.com>
2021-10-20 18:26:12 +0200

    update testsuite for settingspanel component

---
+++ client/src/components/settingsPanel/SettingsPanel.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { render, screen } from '../../scripts/utils';
+import SettingsPanel from './SettingsPanel';
+import '../../scripts/i18n';
+
+describe('SearchBar component', () => {
+    it('should render', () => {
+        render( <SettingsPanel /> );
+    });
+    it('should render children components', () => {
+        const button = document.createElement('button');
+        button.setAttribute('data-testid', 'button');
+        render( 
+            <SettingsPanel>
+                <button data-testid='xyz'>xyz</button>
+            </SettingsPanel>        
+        );
+
+        expect(screen.getByTestId('xyz')).toBeTruthy();
+        expect(screen.getByText('xyz').innerHTML ).toBe('xyz');
+    });
+});
