ba654b1ddf9243f45ad7b703d1707712a51ae580
KevvKo <kklein915@gmail.com>
2021-10-21 20:24:57 +0200

    update testsuite for Friendslist component

--- client/src/components/friendsList/FriendsList.test.js
+++ client/src/components/friendsList/FriendsList.test.js
@@ -1,8 +1,23 @@
-import { render, screen } from '@testing-library/react';
-import FriendsList from './FriendsList.test';
+import React from 'react';
+import { render, screen } from '../../scripts/utils';
+import FriendsList from './FriendsList';
+import '../../scripts/i18n';
 
-// test('renders learn react link', () => {
-//   render(<FriendsList />);
-//   const linkElement = screen.getByText(/ xyz /i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+describe('FriendsList component', () => {
+    it('should render', () => {
+        render( <FriendsList /> );
+    });
+    it('should render friendtabs', () => {
+        render( <FriendsList /> );
+        expect(screen.queryByText(/Friends/i)).toBeTruthy();
+        expect(screen.queryByText(/Nitro/i)).toBeTruthy();
+    });
+    it('should render title', () => {
+        render( <FriendsList /> );
+        expect(screen.queryByText(/Direct messages/i)).toBeTruthy();
+    });
+    it('should render icon button', () => {
+        render( <FriendsList /> );
+        expect(screen.queryAllByTestId(/icon-button/i)).toBeTruthy();
+    });
+});
