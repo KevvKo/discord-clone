4756f561afe10393827dd172b8fa20b5b335a83a
KevvKo <kklein915@gmail.com>
2021-10-23 01:13:43 +0200

    update testsuite for usertag component

--- client/src/components/user/userTag/UserTag.test.js
+++ client/src/components/user/userTag/UserTag.test.js
@@ -1,6 +1,42 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
 import UserTag from './UserTag';
+import { Provider } from 'react-redux';
+import renderer from 'react-test-renderer';
+import configureStore from 'redux-mock-store';
+import { act } from 'react-dom/test-utils';
+const mockStore = configureStore([]);
+ 
+describe('UserAvatar', () => {
+    let store; 
+    let component;
+    beforeEach(() => {
+        store = mockStore({
+            user: {
+                id: 1,
+                name: 'xyz name',
+                email: 'xyz@mail.com',
+                phoneNumber: '12345678',
+                status: true,
+                active: true 
 
-test('renders learn react link', () => {
+            }
+        });
 
+        component = renderer.create( 
+            <Provider store={store}>
+                <UserTag />
+            </Provider>
+        );
+    });
+    it('should render with given state from redux store', () => {
+        expect(component.toJSON()).toMatchSnapshot();
+        act( () => {
+            expect(
+                component.root.findAllByType('div')[1].props.children
+            ).toEqual('xyz name');
+            expect(
+                component.root.findAllByType('div')[2].props.children
+            ).toEqual(1);
+        });
+    });
 });
